32683e3bb4e267b2239f6ceda370c5f867076ef5
Mariana Sobreiro <marianasobreiro93@gmail.com>
2022-04-23 13:06:42 -0300

    Home component estilizado + router links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -10,6 +14,7 @@ import { LoaderComponent } from './components/UI/loader/loader.component';
 import { ListPetsComponent } from './components/list-pets/list-pets.component';
 import { AgendaComponent } from './components/agenda/agenda.component';
 import { AdoptionComponent } from './components/adoption/adoption.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -24,7 +29,11 @@ import { AdoptionComponent } from './components/adoption/adoption.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
